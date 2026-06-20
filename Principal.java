589fa07ab4e3b4971d56e9db005dfdb7ab5d8a9b
Jose Antonio Fernández Gago <joseantoniofernandezgago@gmail.com>
2026-06-20 14:31:03 +0000

    Añadida función experimental de envíos internacionales

--- Principal.java
+++ Principal.java
@@ -1,5 +1,6 @@
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Sincronizado con GitHub Cloud");
+        System.out.println("PROBANDO: Nueva función de envíos internacionales v2");
     }
 }
